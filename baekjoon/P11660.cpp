0d24d7ecdbdc96678d56fe370c19ae934615cf50
kannikii <kannikii0124@gmail.com>
2025-12-22 01:03:45 +0900

    11660번

---
+++ baekjoon/P11660.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N,M;
+    cin>>N>>M;
+    int a[1025][1025]={};
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cin>>a[i][j];
+            //합 배열 구하기
+
+        }
+    }
+    for(int i=0;i<M;i++){
+        int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        //합 배열에서 값 뽑아내기
+    }
+
+
+
+}
