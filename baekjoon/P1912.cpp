2fc5e3eb98ce37d959fd013962cc65c731f71ea9
kannikii <kannikii0124@gmail.com>
2026-02-07 20:36:17 +0900

    1912번 동적계획법 (연속합1 (Kadane's Algorithm))

--- baekjoon/P1912.cpp
+++ baekjoon/P1912.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+static int sqc[100001];
+static int D[100001];
+// D[i] i번째로 끝나는(i포함i가 마지막인) 부분 행렬의 최댓값 
+
 int main() {
     int N;
-    cin>>N;
-    while(N--){
-        cin>>
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> sqc[i];
     }
+    D[1]=sqc[1];
+    long long sum_max=sqc[1];
+    for(int i=2;i<=N;i++){
+        D[i]=max(D[i-1]+sqc[i],sqc[i]);
+        if(sum_max<D[i]){
+            sum_max=D[i];
+        }
+    }
+    cout<<sum_max<<"\n";
 }
