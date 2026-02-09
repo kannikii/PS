f246f23a5235e51e285f705517cc5ab624bcc8bb
kannikii <kannikii0124@gmail.com>
2026-02-10 00:13:16 +0900

    11660번 동적계획법 (누적합)

--- baekjoon/P11660.cpp
+++ baekjoon/P11660.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 using namespace std;
-
-int main(){
+static int a[1025][1025] = {};
+static int S[1025][1025] = {};
+void prefixSumDP(int x1, int y1, int x2, int y2);
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N,M;
-    cin>>N>>M;
-    int a[1025][1025]={};
-    int S[1025][1025]={};
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
-            cin>>a[i][j];
-            //합 배열 구하기
-            S[i][j]=S[i][j-1]+a[i][j];
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> a[i][j];
         }
     }
-    for(int i=1;i<=M;i++){
-        int x1,y1,x2,y2;
-        cin>>x1>>y1>>x2>>y2;
-        //합 배열에서 값 뽑아내기
-        int sum=0;
-        for(int yl=y1;yl<=y2;yl++){
-            sum+=S[yl][x2]-S[yl][x1-1];
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + a[i][j];
         }
-        cout<<sum<<"\n";
     }
+    for (int i = 1; i <= M; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        // 합 배열에서 값 뽑아내기
+        prefixSumDP(x1, y1, x2, y2);
+    }
+}
+void prefixSumDP(int x1, int y1, int x2, int y2) {
+    cout << S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1]
+         << "\n";
 }
