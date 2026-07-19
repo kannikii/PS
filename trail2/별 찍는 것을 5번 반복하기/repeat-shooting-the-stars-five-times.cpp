47689af4299b78e33bab06a88a7e82376136b600
KWON HYOUNG LEE <2020110574@student.dongguk.edu>
2026-07-19 19:16:27 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (1ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void printStar(){
+    for(int i=0;i<10;i++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+int main() {
+    // Please write your code here.
+    printStar();
+    printStar();
+    printStar();
+    printStar();
+    printStar();
+    return 0;
+}
