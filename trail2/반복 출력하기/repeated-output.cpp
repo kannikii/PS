38b767dc373ec67d57c4e0cb72832f8d930e7170
KWON HYOUNG LEE <2020110574@student.dongguk.edu>
2026-07-19 19:17:51 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printS(int N){
+    for(int i=0;i<N;i++){
+        cout<<"12345^&*()_"<<"\n";
+    }
+    
+}
+int main() {
+    // Please write your code here.
+    int N;
+    cin>>N;
+
+    printS(N);
+    return 0;
+}
